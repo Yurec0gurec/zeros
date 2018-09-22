fdbbca498704d3d3e435abe6f0b88eb7cec3e07d
Yuri Kovalev <yurec.0gurec99@gmail.com>
2018-09-22 18:21:49 +0300

    wow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
+    var answer = 0;
+    while(number > 0) {
+        number /= 5;
+        answer = Math.floor(answer);
+        answer += number;
+    }
+   
+ 
+    return answer;
   // your implementation
 }
